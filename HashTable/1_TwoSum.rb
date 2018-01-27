bdfca9fb6d479101c2fad7066fe6be5ff432577f
ConradGcn <conradgcn@hotmail.com>
2018-01-27 20:25:00 +0800

    Update 1_TwoSum.rb

--- HashTable/1_TwoSum.rb
+++ HashTable/1_TwoSum.rb
@@ -3,7 +3,7 @@
 #  https://leetcode.com/problems/two-sum/description
 # 
 #  Language: Ruby
-#  Auther: ConradG
+#  Author: ConradG
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 #   第一反应为双循环遍历，发现题目归类于 Hash Table 中后也尝试使Hash Table 进行处理。
 # 在单线程场景下，使用Hash的方式有更优秀的时间复杂度，而双循环则更易拆分为多个线程。
@@ -20,3 +20,10 @@ def two_sum(nums, target)
     num, i = nums.each_with_index.detect {|(num, i)| s[target - num] || !s[num] = i}
     [s[target - num], i]
 end
+
+#
+# 19 / 19 test cases passed.
+# Status: Accepted
+# Runtime: 57 ms
+# Your runtime beats 75.07 % of ruby submissions.
+#
