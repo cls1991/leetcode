e22a88595af19081401c7ddcfb5d86c6b597a0e2
cls1991 <tzk19910406@hotmail.com>
2018-02-05 21:01:29 +0800

    旋转数组查找(二分搜索, 注意边界确定)

---
+++ BinarySearch/33_SearchInRotatedSortedArray.py
@@ -0,0 +1,47 @@
+# coding: utf8
+
+
+"""
+    题目链接: https://leetcode.com/problems/search-in-rotated-sorted-array/description.
+    题目描述:
+
+    Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+    (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+    You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+    You may assume no duplicate exists in the array.
+
+"""
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+
+        left = 0
+        right = len(nums) - 1
+        # 每次确定一半有序
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < nums[right]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+        return -1
